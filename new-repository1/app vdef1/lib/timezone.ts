884f4f11831bb623ab280ced9d2dabfef75cebfe
cquinsar-code <cquinsar@gmail.com>
2026-01-21 09:46:56 +0000

    Delete new-repository1/app vdef1 directory

--- new-repository1/app vdef1/lib/timezone.ts
+++
@@ -1,7 +0,0 @@
-import { zonedTimeToUtc } from "date-fns-tz";
-
-export const CANARY_TZ = "Atlantic/Canary";
-
-export function nowCanary(): Date {
-  return zonedTimeToUtc(new Date(), CANARY_TZ);
-}
