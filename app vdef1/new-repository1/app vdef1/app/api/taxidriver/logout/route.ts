2acac17f01ffd62ed057b7f0f16d7dd00e9e3053
cquinsar-code <cquinsar@gmail.com>
2026-01-21 09:37:49 +0000

    Delete app vdef1 directory

--- app vdef1/new-repository1/app vdef1/app/api/taxidriver/logout/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(req: Request) {
-  const response = NextResponse.json({ success: true });
-  response.cookies.delete("taxista_session"); // elimina cookie de sesión
-  return response;
-}
